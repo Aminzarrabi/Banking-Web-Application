057cde69ff8a969cee969fb43840eb8c4790f8b5
Aminzarrabi <aminzarrabi0@gmail.com>
2026-01-28 10:46:56 +0330

    accounts added

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
 
 // username : js, pin :1111
 // username : jd, pin :2222
+// username : hs, pin :3333
+// username : ar, pin :4444
 
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
@@ -29,7 +31,7 @@ const account1 = {
     '2020-07-12T10:51:36.790Z',
   ],
   currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -52,7 +54,49 @@ const account2 = {
   locale: 'en-US',
 };
 
-const accounts = [account1, account2];
+const account3 = {
+  owner: 'Hiroyuki Sanada',
+  movements: [1200, 1450, -908, 5000, -6140, 163.9, 677, -400],
+  interestRate: 1.6, // %
+  pin: 3333,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'JPY',
+  locale: 'ja-Jpan',
+};
+
+const account4 = {
+  owner: 'Amin Rostami',
+  movements: [
+    1200000, 1450000, -98000, 5000000, -6140000, 163000, 677000, -400000,
+  ],
+  interestRate: 1.6, // %
+  pin: 4444,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'IRR',
+  locale: 'fa-IR',
+};
+
+const accounts = [account1, account2, account3, account4];
 
 /////////////////////////////////////////////////
 // Elements
